fc4cd8ac82340f2a70f77661296aecf5a68c623d
Sam Crouch <stcrouch17@gmail.com>
2021-06-29 09:56:10 -1000

    knex walkthrough completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const knex = require('knex')(require('path/to/knexfile.js')[process.env.NODE_ENV]);
+const knex = require('knex')(require('~/JS/knex-exercise/knexfile.js')[process.env.NODE_ENV]);
 
 app.use(bodyParser.json());
 
